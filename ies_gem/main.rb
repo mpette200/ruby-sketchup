c90419251e5bc200c78081d9853028f036c9de94
mpette200 <mpette200@gmail.com>
2021-02-08 08:42:20 +0000

    different layer types

--- ies_gem/main.rb
+++ ies_gem/main.rb
@@ -3,41 +3,41 @@
 # load 'ies_gem/main.rb'
 require 'sketchup.rb'
 
+# [0..-2] removes newline at end
+LAYER_ROOM = <<HEREDOC[0..-2]
+LAYER
+1
+COLOUR
+0
+CATEGORY
+1
+TYPE
+1
+SUBTYPE
+2001
+COLOURRGB
+16711680
+HEREDOC
+
+# [0..-2] removes newline at end
+LAYER_SHADE = <<HEREDOC[0..-2]
+LAYER
+64
+COLOUR
+0
+CATEGORY
+1
+TYPE
+4
+SUBTYPE
+0
+COLOURRGB
+65280
+HEREDOC
+
 module MPETT
   module IESGEM
-    def self.output(group, out)
-      # Intro Start
-      
-      # COM GEM data file created by MP
-      # LAYER
-      # 1
-      # COLOUR
-      # 0
-      # CATEGORY
-      # 1
-      # TYPE
-      # 1
-      # SUBTYPE
-      # 2001
-      # COLOURRGB
-      # 16711680
-      # IES *** name ***
-
-      # intro clauses
-      out.push("LAYER")
-      out.push("1")
-      out.push("COLOUR")
-      out.push("0")
-      out.push("CATEGORY")
-      out.push("1")
-      out.push("TYPE")
-      out.push("1")
-      out.push("SUBTYPE")
-      out.push("2001")
-      out.push("COLOURRGB")
-      out.push("16711680")
-      out.push("IES #{group.name}")
-      
+    def self.output(group, out, layer_text)
       # turn group into ies gemfile
       # Nvertices Nfaces 0
       #    x1    y1    z1
@@ -47,6 +47,11 @@ module MPETT
       # 0
       # Nvertices v1 v2 v3 
       # 0
+
+      # layer definitions
+      out.push(layer_text)
+      out.push("IES #{group.name}")
+      
       vertices = []
       faces = []
       vcheck = {} # hash table to lookup index of vertex
@@ -86,14 +91,31 @@ module MPETT
       return "done"
     end # output
 
-    def self.activate
+    def self.process_rooms
       out = []
       out.push("COM GEM data file created by MP")
       model = Sketchup.active_model
       selection = model.selection
       selection.each { |g|
         if g.is_a? Sketchup::Group
-          output(g, out)
+          output(g, out, LAYER_ROOM)
+          if !g.manifold?
+            msg = "#{g.name} is not a closed solid. Try solid inspector"
+            UI.messagebox(msg)
+            raise TypeError, msg
+          end
+        end
+      }
+      puts "#{out.join("\n")}"
+    end
+
+    def self.process_shade
+      out = []
+      model = Sketchup.active_model
+      selection = model.selection
+      selection.each { |g|
+        if g.is_a? Sketchup::Group
+          output(g, out, LAYER_SHADE)
         end
       }
       puts "#{out.join("\n")}"
@@ -102,7 +124,10 @@ module MPETT
     unless file_loaded?(__FILE__)
       menu = UI.menu('Plugins')
       menu.add_item('IES gemfile') {
-        self.activate
+        self.process_rooms
+      }
+      menu.add_item('IES gemfile_shade') {
+        self.process_shade
       }
       file_loaded(__FILE__)
     end
